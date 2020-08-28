77e588a3f6215aef040a137e50f8b812017b27d7
George <kniazyouk@gmail.com>
2020-08-27 20:45:10 -0400

    second commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  fac: string;
+  IVA = 0;
+  IT = 0;
+  total = 0;
+  utilidad = 0;
+  df_IVA: string;
+
+
+
   constructor() {}
+
+  calcular(){
+
+    this.IVA = parseInt(this.fac)  * 0.13;
+
+    this.IT = parseInt(this.fac) * 0.03;
+    
+    this.total = this.IVA + this.IT;
+    
+    this.utilidad = parseInt(this.fac) - this.total
+  }
 
 }
